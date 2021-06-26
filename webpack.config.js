0d9467d485a359862ba253e94612407339e9452e
sangey <sangeylama1000@gmail.com>
2021-06-26 11:18:14 +0545

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: 'https://tenzin1000.github.io/trek-nepal/',
     filename: 'build.js'
   },
   module: {
